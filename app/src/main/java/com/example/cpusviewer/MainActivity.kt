98ad2fd2e0146ee8f7f4dfbda70aa283f20edc80
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2023-07-11 18:37:22 +0200

    feat: randomize images in each CPU

--- app/src/main/java/com/example/cpusviewer/MainActivity.kt
+++ app/src/main/java/com/example/cpusviewer/MainActivity.kt
@@ -13,23 +13,129 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root // binding variable
         setContentView(view) // binding variable
 
-        val cpus = mutableListOf<CPUsItem>(
+        val cpus = mutableListOf(
             CPUsItem(
-                "this cpu is made by intel and is a 64 bit cpu",
+                "This CPU is made by Intel and is a 64-bit CPU.",
                 1,
-                "https://t3.ftcdn.net/jpg/02/81/42/78/240_F_281427849_nRups6kcaGgVVWC5tF0qQks03qq7cxAF.jpg",
-                "8068 CPU"),
+                "https://picsum.photos/200?random=1",
+                "8068 CPU"
+            ),
             CPUsItem(
-                "this cpu is made by AMD and is a 64 bit cpu",
+                "This CPU is made by AMD and is a 64-bit CPU.",
                 2,
-                "https://t4.ftcdn.net/jpg/01/36/83/05/240_F_136830578_Uk1mQk9MGSHbOZpjiTnz7JxAOHKCwVKH.jpg",
-                "AMD Ryzen 5 3600"),
+                "https://picsum.photos/200?random=2",
+                "AMD Ryzen 5 3600"
+            ),
             CPUsItem(
-                "this cpu is made by Atmega and is a 32 bit cpu",
+                "This CPU is made by Intel and is a 32-bit CPU.",
                 3,
-                "https://t4.ftcdn.net/jpg/01/09/59/81/240_F_109598141_hhHRriyaO2jIDUPbttE385zyEZUsrHbP.jpg",
-                "Intel Core i9-9900K"),
+                "https://picsum.photos/200?random=3",
+                "Intel Core i9-9900K"
+            ),
+            CPUsItem(
+                "This CPU is made by AMD and is a 64-bit CPU.",
+                4,
+                "https://picsum.photos/200?random=4",
+                "AMD Ryzen 7 5800X"
+            ),
+            CPUsItem(
+                "This CPU is made by Intel and is a 64-bit CPU.",
+                5,
+                "https://picsum.photos/200?random=5",
+                "Intel Core i7-11700K"
+            ),
+            CPUsItem(
+                "This CPU is made by AMD and is a 64-bit CPU.",
+                6,
+                "https://picsum.photos/200?random=6",
+                "AMD Ryzen 9 5950X"
+            ),
+            CPUsItem(
+                "This CPU is made by Intel and is a 64-bit CPU.",
+                7,
+                "https://picsum.photos/200?random=7",
+                "Intel Core i5-11600K"
+            ),
+            CPUsItem(
+                "This CPU is made by AMD and is a 64-bit CPU.",
+                8,
+                "https://picsum.photos/200?random=8",
+                "AMD Ryzen 3 3300X"
+            ),
+            CPUsItem(
+                "This CPU is made by Intel and is a 64-bit CPU.",
+                9,
+                "https://picsum.photos/200?random=9",
+                "Intel Core i3-10100"
+            ),
+            CPUsItem(
+                "This CPU is made by AMD and is a 64-bit CPU.",
+                10,
+                "https://picsum.photos/200?random=10",
+                "AMD Ryzen 5 5600X"
+            ),
+            CPUsItem(
+                "This CPU is made by Intel and is a 64-bit CPU.",
+                11,
+                "https://picsum.photos/200?random=11",
+                "Intel Core i9-11900K"
+            ),
+            CPUsItem(
+                "This CPU is made by AMD and is a 64-bit CPU.",
+                12,
+                "https://picsum.photos/200?random=12",
+                "AMD Ryzen 9 5900X"
+            ),
+            CPUsItem(
+                "This CPU is made by Intel and is a 64-bit CPU.",
+                13,
+                "https://picsum.photos/200?random=13",
+                "Intel Core i7-10700K"
+            ),
+            CPUsItem(
+                "This CPU is made by AMD and is a 64-bit CPU.",
+                14,
+                "https://picsum.photos/200?random=14",
+                "AMD Ryzen 7 3800XT"
+            ),
+            CPUsItem(
+                "This CPU is made by Intel and is a 64-bit CPU.",
+                15,
+                "https://picsum.photos/200?random=15",
+                "Intel Core i5-11400F"
+            ),
+            CPUsItem(
+                "This CPU is made by AMD and is a 64-bit CPU.",
+                16,
+                "https://picsum.photos/200?random=16",
+                "AMD Ryzen 5 3500X"
+            ),
+            CPUsItem(
+                "This CPU is made by Intel and is a 64-bit CPU.",
+                17,
+                "https://picsum.photos/200?random=17",
+                "Intel Core i3-9100F"
+            ),
+            CPUsItem(
+                "This CPU is made by AMD and is a 64-bit CPU.",
+                18,
+                "https://picsum.photos/200?random=18",
+                "AMD Ryzen 3 3100"
+            ),
+            CPUsItem(
+                "This CPU is made by Intel and is a 64-bit CPU.",
+                19,
+                "https://picsum.photos/200?random=19",
+                "Intel Core i9-10900K"
+            ),
+            CPUsItem(
+                "This CPU is made by AMD and is a 64-bit CPU.",
+                20,
+                "https://picsum.photos/200?random=20",
+                "AMD Ryzen 9 3900XT"
+            )
         )
+
         val adaptor = CPUAdaptor(cpus)
         binding.rvCpus.adapter = adaptor
         binding.rvCpus.layoutManager = LinearLayoutManager(this)
